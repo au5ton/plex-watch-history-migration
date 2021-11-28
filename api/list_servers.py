fe0f08b89d82ea98ed7e641e9e0d53ffa98f9991
Austin Jackson <themacphage@gmail.com>
2021-11-27 20:54:14 -0600

    json encode

--- api/list_servers.py
+++ api/list_servers.py
@@ -1,6 +1,6 @@
 import os
+import json
 from http.server import BaseHTTPRequestHandler
-#from datetime import datetime
 from urllib import parse
 from plexapi.myplex import MyPlexAccount
 
@@ -9,11 +9,10 @@ class handler(BaseHTTPRequestHandler):
   def do_GET(self):
     query = dict(parse.parse_qsl(parse.urlsplit(self.path).query))
     self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
+    self.send_header('Content-type', 'application/json')
     self.end_headers()
-    #response_body = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     response_body = list_servers(query["plex_token"])
-    self.wfile.write(str(response_body).encode())
+    self.wfile.write(str(json.dumps(response_body)).encode())
     return
 
 def list_servers(plex_token: str) -> list[str]:
