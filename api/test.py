459d8e2d06ac07864c9681d09555450ebe963a24
Austin Jackson <themacphage@gmail.com>
2021-11-27 20:19:55 -0600

    test

--- api/test.py
+++ api/test.py
@@ -2,10 +2,13 @@ from http.server import BaseHTTPRequestHandler
 from datetime import datetime
 from plexapi.myplex import MyPlexAccount
 
+from ..lib import util
+
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     print(MyPlexAccount)
+    util.do_thing()
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
---
+++ lib/util.py
@@ -0,0 +1,7 @@
+def do_thing():
+  print("hello")
+
+
+
+if __name__ == "__main__":
+  print("main")
