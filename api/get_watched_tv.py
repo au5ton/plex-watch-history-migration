c03f76d89095541418ce527fdd13d3064895c361
Austin Jackson <themacphage@gmail.com>
2021-11-28 19:48:29 -0600

    added get_watched_tv and updated get_watched_movies

--- api/get_watched_tv.py
+++ api/get_watched_tv.py
@@ -3,10 +3,10 @@ import json
 import jsonpickle
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-from plexapi.library import MovieSection
+from plexapi.library import ShowSection
 from plexapi.myplex import MyPlexAccount
 from plexapi.server import PlexServer
-from plexapi.video import Movie
+from plexapi.video import Episode
 
 class handler(BaseHTTPRequestHandler):
 
@@ -16,21 +16,25 @@ class handler(BaseHTTPRequestHandler):
     self.send_header('Content-type', 'application/json')
     self.send_header('Access-Control-Allow-Origin', '*')
     self.end_headers()
-    response_body = get_watched_movies(query["plex_token"], query["server_name"])
+    response_body = get_watched_tv(query["plex_token"], query["server_name"])
     self.wfile.write(str(jsonpickle.encode(response_body)).encode())
     return
 
 # see: https://stackoverflow.com/a/8187203
-class WatchedMovieDTO:
-  title = str()
+class WatchedEpisodeDTO:
+  grandparentTitle = str() # Name of the show for the episode
+  parentIndex = int() # Season number of episode
+  index = int() # Episode number
+  formatted = str()
   guid = str()
   def __init__(self, **kwargs):
     for key, value in kwargs.items():
       setattr(self, key, value)
+    self.formatted = f'{self.grandparentTitle} - S{str(self.parentIndex).zfill(2)}E{str(self.index).zfill(2)}'
   def __repr__(self):
     return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True)
 
-def get_watched_movies(plex_token: str, server_name: str) -> list[WatchedMovieDTO]:
+def get_watched_tv(plex_token: str, server_name: str) -> list[WatchedEpisodeDTO]:
   account = MyPlexAccount(token=plex_token)
   # only the server we're looking for
   servers = [item for item in account.resources() if item.product == 'Plex Media Server' and item.name == server_name]
@@ -38,16 +42,23 @@ def get_watched_movies(plex_token: str, server_name: str) -> list[WatchedMovieDT
     # first server with matching name
     server = servers[0]
     plex: PlexServer = server.connect()
-    results: list[WatchedMovieDTO] = []
+    results: list[WatchedEpisodeDTO] = []
     # only "movie" libraries
-    sections: list[MovieSection] = [item for item in plex.library.sections() if item.TYPE == 'movie']
+    sections: list[ShowSection] = [item for item in plex.library.sections() if item.TYPE == 'show']
     for section in sections:
-      movies: list[Movie] = section.search(unwatched=False, libtype='movie', sort='titleSort', includeGuids=True, maxresults=99999999)
-      results += [WatchedMovieDTO(title=item.title, guid=item.guid) for item in movies]
+      episodes: list[Episode] = section.search(unwatched=False, libtype='episode', includeGuids=True, maxresults=99999999)
+      results += [
+        WatchedEpisodeDTO(
+          grandparentTitle=item.grandparentTitle,
+          parentIndex=item.parentIndex,
+          index=item.index,
+          guid=item.guid)
+        for item in episodes
+      ]
     return results
   else:
     return []
 
 if __name__ == "__main__":
   #print(get_watched_movies(plex_token=os.environ["X_PLEX_TOKEN"], server_name="mars"))
-  print(str(jsonpickle.encode(get_watched_movies(plex_token=os.environ["X_PLEX_TOKEN"], server_name="mars"), indent=2)))
+  print(str(jsonpickle.encode(get_watched_tv(plex_token=os.environ["X_PLEX_TOKEN"], server_name="mars"), indent=2)))
